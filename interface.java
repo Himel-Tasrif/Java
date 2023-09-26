616eb5ea40e9df131dbe858bd203eb910ed8f223
Himel-Tasrif <himel35-1078@diu.edu.bd>
2023-09-26 22:38:25 +0600

    Interface

---
+++ interface.java
@@ -0,0 +1,29 @@
+abstract class Shape {
+    public abstract double calculateArea();
+}
+
+interface Drawable {
+    void draw();
+}
+
+class Circle extends Shape implements Drawable {
+    private double r;
+
+    public Circle(double r) {
+        this.r = r;
+    }
+    public double calculateArea() {
+        return 3.1416 * r * r;
+    }
+    public void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
+
+public class interface {
+    public static void main(String[] args) {
+        Circle obj1 = new Circle(5.0);
+        System.out.println("Area of the circle: " + obj1.calculateArea());
+        obj1.draw();
+    }
+}
