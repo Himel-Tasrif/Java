de951713fa08d43005e7f8d8c2f003a6ac45f58b
Himel-Tasrif <himel35-1078@diu.edu.bd>
2023-10-09 22:56:43 +0600

    Static

---
+++ static_key.java
@@ -0,0 +1,15 @@
+// Tasrif Nur Himel
+
+/*
+                                          *** Static Variable *** 
+
+ */
+
+class student{
+    String name;
+    int id;
+    static  String varsityName = "Daffodil International University"; // Static key word
+}
+public class static_key {
+    
+}
