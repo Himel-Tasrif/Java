d1d4bf983664a58293fb45947fad446d36223334
Himel-Tasrif <himel35-1078@diu.edu.bd>
2023-10-09 23:27:08 +0600

    Static Keyword

--- static_key.java
+++ static_key.java
@@ -10,16 +10,34 @@ The static keyword belongs to the class than instance of the class.
 
  */
 
-class student{
+
+ // Class name should start with an uppercase letter
+class Student { 
     String name;
     int id;
-    static  String varsityName = "Daffodil International University"; // Static key word
+    static String varsityName = "Daffodil International University"; // Static keyword
+
+    // Constructor
+    Student(String n, int i) {
+        name = n;
+        id = i;
+    }
+
+    // Method for print
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("University Name: " + varsityName + "\n");
+    }
 }
 
-//construct a constructor:
-void student(String n, int i){
-
-}
 public class static_key {
-    
+    public static void main(String[] args) {
+        Student obj1 = new Student("Tasrif Nur Himel", 22135);
+        Student obj2 = new Student("Busrat Jahan", 1078);
+
+        obj1.display();
+        obj2.display();
+    }
 }
+
