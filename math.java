3ea442c605f25d023071c995f668db5c17a81d9f
Himel-Tasrif <himel35-1078@diu.edu.bd>
2023-10-13 06:13:27 +0600

    Math Class

---
+++ math.java
@@ -0,0 +1,14 @@
+public class math {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-9.5));
+        System.out.println(Math.sqrt(25.5));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.log(2.0));
+        System.out.println(Math.exp(1.0));
+        System.out.println(Math.max(1,2));
+        System.out.println(Math.min(1,2));
+        System.out.println(Math.ceil(1.5));
+        System.out.println(Math.floor(1.5));
+        System.out.println(Math.PI);
+    }
+}
